7b4525c0e9676563380f9335ac07266910cf13f8
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-08-30 16:27:23 -0300

    Add loading and error states to video page

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
-    
+  <div v-if="isLoadingGeneral">
+    <LoadingSpinner />
+  </div>
+  <div v-else-if="!video">
+    <v-container>
+      <v-alert type="error">Failed to load video information.</v-alert>
+    </v-container>
+  </div>
+  <div v-else>
+    <VideoPlayer :stream-url="video.hls" :captions-url="`/api/captions/${videoId}`" />
   </div>
 </template>
 
 <script lang="ts" setup>
+const route = useRoute();
+const { getVideo } = useVideo();
 
+const videoId = ref<string>(route.params.id as string);
+const isLoadingGeneral = ref<boolean>(true);
+const video = ref<Video | null>(null);
+
+const loadVideo = async () => {
+  isLoadingGeneral.value = true;
+  try {
+    video.value = await getVideo(videoId.value);
+  } catch (error) {
+    console.error('Error loading video:', error);
+  } finally {
+    isLoadingGeneral.value = false;
+  }
+};
+
+onMounted(async () => {
+  await loadVideo();
+});
 </script>
-
-<style>
-
-</style>
