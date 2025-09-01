b3262ef6811df61741015af8f71bba030e8adaff
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 14:06:02 -0300

    Add explicit icon to video thumbnail titles

--- app/components/ArtistVideoThumbnail.vue
+++ app/components/ArtistVideoThumbnail.vue
@@ -1,10 +1,10 @@
 <template>
   <v-card class="mx-auto" max-width="320" :link="true" hover :to="`/video/${video.id}`">
     <v-img :src="video.thumbnail" :alt="`Thumbnail for ${video.title}`" height="180px" />
-    <v-card-title class="text-truncate text-center" :title="video.title">{{ video.title }}</v-card-title>
-    <v-card-subtitle class="text-center">
-      {{ formatArtist(video.artists) }}
-    </v-card-subtitle>
+    <v-card-title class="text-truncate text-center" :title="video.title">
+      <v-icon v-if="video.explicit" class="alpha-e-box" size="24" icon="mdi-alpha-e-box" />
+      {{ video.title }}
+    </v-card-title>
     <v-card-subtitle class="text-center">
       {{ video.viewCounts.total.toLocaleString() }} views • {{ formatDuration(video.duration) }}
     </v-card-subtitle>
@@ -15,15 +15,4 @@
 const props = defineProps<{
   video: ArtistVideo;
 }>();
-
-const formatArtist = (artists: VideoArtist[]): string => {
-  return artists.map(artist => artist.artist.name).join(', ');
-};
-
-const formatDuration = (milliseconds: number): string => {
-  let totalSeconds = Math.floor(milliseconds / 1000);
-  let minutes = Math.floor(totalSeconds / 60);
-  let seconds = totalSeconds % 60;
-  return String(minutes).padStart(2, '0') + ":" + String(seconds).padStart(2, '0');
-};
 </script>
