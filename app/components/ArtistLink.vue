6265a362d031ce7a6677b7cbb2474d57a3250884
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 14:06:08 -0300

    Create ArtistLink.vue

---
+++ app/components/ArtistLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink v-for="(artist, index) in videoArtists" :key="artist.artist.name" :to="`/artist/${artist.artist.id}`">
+    <v-chip v-if="useChips">
+      {{ artist.artist.name }}
+    </v-chip>
+    <span v-else>
+      {{ artist.artist.name }}<span v-if="index < videoArtists.length - 1">, </span>
+    </span>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  videoArtists: VideoArtist[];
+  useChips?: boolean;
+}>();
+</script>
