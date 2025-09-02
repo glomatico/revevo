4edf0b52bf42ac413b71af5af098f55d9abccd0c
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 08:17:46 -0300

    Update homepage introduction and project info

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,23 @@
       Welcome!
     </h1>
     <p>
-      This is a still under-construction Vevo client. Start by searching for a video using the search bar above!
+      Welcome to Revevo - a project that aims to bring the classic Vevo website back to life.
+    </p>
+    <p>
+      The original Vevo website shut down years ago, but their internal servers are still online (although not fully up
+      to date). Using the still-active
+      APIs
+      from the Vevo TV
+      app, Revevo reconstructs the old Vevo experience: music videos, curated playlists, and artist pages, all
+      rebuilt for a desktop/mobile web experience.
+    </p>
+    <p>
+      Go ahead and start searching for your favorite videos using the search bar above!
+    </p>
+    <v-divider thickness="2" class="my-2" />
+    <p>
+      This project is open source, and you can find the code on
+      <a href="https://github.com/glomatico/revevo" target="_blank">GitHub</a>.
     </p>
   </v-container>
 </template>
