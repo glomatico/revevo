12dc3621c8c746bbe82060c56470b635058666a5
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 21:39:05 -0300

    Update ArtistVideoThumbnail layout and image size

--- app/components/ArtistVideoThumbnail.vue
+++ app/components/ArtistVideoThumbnail.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-card class="mx-auto" max-width="320" :link="true" hover :to="`/video/${video.id}`">
-    <v-img :src="video.thumbnail" :alt="`Thumbnail for ${video.title}`" height="180px" />
-    <v-card-title class="text-truncate text-center" :title="video.title">
-      <v-icon v-if="video.explicit" class="alpha-e-box" size="24" icon="mdi-alpha-e-box" />
-      {{ video.title }}
-    </v-card-title>
-    <v-card-subtitle class="text-center">
-      {{ video.viewCounts.total.toLocaleString() }} views • {{ formatDuration(video.duration) }}
-    </v-card-subtitle>
+  <v-card class="mx-auto" :link="true" hover :to="`/video/${video.id}`">
+    <v-img :src="video.thumbnail" :alt="`Thumbnail for ${video.title}`" height="150px" cover />
+    <v-card-text class="pa-2">
+      <v-card-title class="text-truncate text-center" :title="video.title">
+        <v-icon v-if="video.explicit" class="alpha-e-box" size="24" icon="mdi-alpha-e-box" />
+        {{ video.title }}
+      </v-card-title>
+      <v-card-subtitle class="text-center">
+        {{ video.viewCounts.total.toLocaleString() }} views • {{ formatDuration(video.duration) }}
+      </v-card-subtitle>
+    </v-card-text>
   </v-card>
 </template>
 
