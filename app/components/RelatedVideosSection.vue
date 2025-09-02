cec58b5fb57dbd06259f5c1b5c0ad0247d49b02e
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 10:52:00 -0300

    Wrap video info and related videos in v-card

--- app/components/RelatedVideosSection.vue
+++ app/components/RelatedVideosSection.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <h2>Up next</h2>
-    </v-col>
+  <v-card flat>
+    <v-card-item>
+      <v-card-title>
+        <h2>Up next</h2>
+      </v-card-title>
+    </v-card-item>
     <v-col cols="12" v-for="item in continuousPlay.items" :key="item.video.id">
       <GenericVideoThumbnail :video="item.video" />
     </v-col>
-  </v-row>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
--- app/components/VideoInfo.vue
+++ app/components/VideoInfo.vue
@@ -1,50 +1,56 @@
 <template>
-  <h2>{{ video.title }}</h2>
-  <v-table fixed-header>
-    <tbody>
-      <tr>
-        <td>Artists</td>
-        <td>
-          <ArtistLink :video-artists="video.artists" :use-chips="true" />
-        </td>
-      </tr>
-      <tr>
-        <td>Views</td>
-        <td>{{ video.viewCounts.total.toLocaleString() }}</td>
-      </tr>
-      <tr>
-        <td>Uploaded At</td>
-        <td>{{ new Date(video.created!).toLocaleString() }}</td>
-      </tr>
-      <tr>
-        <td>Genre</td>
-        <td>
-          {{ video.genre }}
-        </td>
-      </tr>
-      <tr>
-        <td>Explicit</td>
-        <td>{{ video.explicit ? 'Yes' : 'No' }}</td>
-      </tr>
-      <tr>
-        <td>Copyright</td>
-        <td>{{ video.copyright }}</td>
-      </tr>
-      <tr>
-        <td>Label</td>
-        <td>{{ video.label }}</td>
-      </tr>
-      <tr>
-        <td>Duration</td>
-        <td>{{ formatDuration(video.duration) }}</td>
-      </tr>
-      <tr>
-        <td>ISRC</td>
-        <td>{{ video.id }}</td>
-      </tr>
-    </tbody>
-  </v-table>
-  <VideoExtraInfo :video="video" />
+  <v-card flat>
+    <v-card-item>
+      <v-card-title>
+        <h2>{{ video.title }}</h2>
+      </v-card-title>
+    </v-card-item>
+    <v-table fixed-header>
+      <tbody>
+        <tr>
+          <td>Artists</td>
+          <td>
+            <ArtistLink :video-artists="video.artists" :use-chips="true" />
+          </td>
+        </tr>
+        <tr>
+          <td>Views</td>
+          <td>{{ video.viewCounts.total.toLocaleString() }}</td>
+        </tr>
+        <tr>
+          <td>Uploaded At</td>
+          <td>{{ new Date(video.created!).toLocaleString() }}</td>
+        </tr>
+        <tr>
+          <td>Genre</td>
+          <td>
+            {{ video.genre }}
+          </td>
+        </tr>
+        <tr>
+          <td>Explicit</td>
+          <td>{{ video.explicit ? 'Yes' : 'No' }}</td>
+        </tr>
+        <tr>
+          <td>Copyright</td>
+          <td>{{ video.copyright }}</td>
+        </tr>
+        <tr>
+          <td>Label</td>
+          <td>{{ video.label }}</td>
+        </tr>
+        <tr>
+          <td>Duration</td>
+          <td>{{ formatDuration(video.duration) }}</td>
+        </tr>
+        <tr>
+          <td>ISRC</td>
+          <td>{{ video.id }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <VideoExtraInfo :video="video" />
+  </v-card>
 </template>
 
 <script lang="ts" setup>
