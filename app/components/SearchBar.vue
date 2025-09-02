4c331bbc88f89c96dd7fe87d18fea88ad7ba4780
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 16:18:45 -0300

    Remove search button from SearchBar component

--- app/components/SearchBar.vue
+++ app/components/SearchBar.vue
@@ -1,11 +1,6 @@
 <template>
   <v-text-field label="Search" prepend-inner-icon="mdi-magnify" outlined dense hide-details width="400px"
     @keyup.enter="search($event.target.value)" v-model="query">
-    <template #append>
-      <v-btn icon @click="search(query)" class="mt-n1">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </template>
   </v-text-field>
 </template>
 
