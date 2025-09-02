e689ee9d4940b764596f84f461cbc87f59a42092
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 21:56:29 -0300

    Update video thumbnail to use 16:9 aspect ratio

--- app/components/ArtistVideoThumbnail.vue
+++ app/components/ArtistVideoThumbnail.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card class="mx-auto" :link="true" hover :to="`/video/${video.id}`">
-    <v-img :src="video.thumbnail" :alt="`Thumbnail for ${video.title}`" height="144px" cover />
+  <v-card :link="true" hover :to="`/video/${video.id}`">
+    <v-img :src="video.thumbnail" :alt="`Thumbnail for ${video.title}`" :aspect-ratio="16 / 9" />
     <v-card-text class="pa-2">
       <v-card-title class="text-truncate text-center" :title="video.title">
         <v-icon v-if="video.explicit" class="alpha-e-box" size="24" icon="mdi-alpha-e-box" />
