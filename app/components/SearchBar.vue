b33f9e67d3135066399a0c27ea3d1e533bcf9d86
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 22:31:26 -0300

    Create SearchBar.vue

---
+++ app/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-text-field label="Search" prepend-inner-icon="mdi-magnify" outlined dense hide-details width="400px"
+    @keyup.enter="search($event.target.value)" v-model="query">
+    <template #append>
+      <v-btn icon @click="search(query)" class="mt-n1">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </template>
+  </v-text-field>
+</template>
+
+<script lang="ts" setup>
+const router = useRouter();
+
+const query = ref<string>('');
+
+const search = async (query: string) => {
+  if (!query.trim()) return;
+  await router.push({ path: '/search', query: { q: query } });
+};
+</script>
