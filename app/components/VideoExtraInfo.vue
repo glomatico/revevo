e4f0cf9778786f6daeacc803fcf5fd5c9f91fc27
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 19:51:32 -0300

    Refactor VideoExtraInfo layout using v-row and v-col

--- app/components/VideoExtraInfo.vue
+++ app/components/VideoExtraInfo.vue
@@ -2,42 +2,46 @@
   <v-btn block variant="text" @click="isExtraInfoDialogVisible = true">Show extra info</v-btn>
   <v-dialog v-model="isExtraInfoDialogVisible" max-width="600px">
     <v-card>
-      <v-card-item>
-        <v-card-title>
-          Extra Info
-        </v-card-title>
-
-        <v-table fixed-header>
-          <tbody>
-            <tr v-if="video.hls">
-              <td>HLS Stream URL</td>
-              <td>
-                <CopyableReadOnlyField :value="video.hls" />
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-if="video.dash">
-            <tr>
-              <td>DASH Stream URL</td>
-              <td>
-                <CopyableReadOnlyField :value="video.dash" />
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-if="video.mp4" v-for="([key, value]) in Object.entries(video.mp4)" :key="key">
-            <tr>
-              <td>MP4 Stream URL ({{ value.quality }})</td>
-              <td>
-                <CopyableReadOnlyField :value="value.url" />
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-card-item>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn variant="text" @click="isExtraInfoDialogVisible = false">Close</v-btn>
-      </v-card-actions>
+      <v-row>
+        <v-col cols="12">
+          <v-card-title>
+            Extra Info
+          </v-card-title>
+        </v-col cols="12">
+        <v-col>
+          <v-table fixed-header>
+            <tbody>
+              <tr v-if="video.hls">
+                <td>HLS Stream URL</td>
+                <td>
+                  <CopyableReadOnlyField :value="video.hls" />
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="video.dash">
+              <tr>
+                <td>DASH Stream URL</td>
+                <td>
+                  <CopyableReadOnlyField :value="video.dash" />
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="video.mp4" v-for="([key, value]) in Object.entries(video.mp4)" :key="key">
+              <tr>
+                <td>MP4 Stream URL ({{ value.quality }})</td>
+                <td>
+                  <CopyableReadOnlyField :value="value.url" />
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+        <v-col cols="12">
+          <v-card-actions>
+            <v-btn variant="text" @click="isExtraInfoDialogVisible = false">Close</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
     </v-card>
   </v-dialog>
 </template>
