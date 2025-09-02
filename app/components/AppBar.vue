94dad3569cc69ece53774066cc7b78b74f3e9689
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-01 22:31:24 -0300

    Add SearchBar component to AppBar

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -1,12 +1,13 @@
 <template>
   <v-toolbar color="primary">
     <v-container>
-      <v-row>
+      <v-row align="center">
         <v-col>
           <v-toolbar-title>
             <h2>Revevo</h2>
           </v-toolbar-title>
         </v-col>
+        <SearchBar />
       </v-row>
     </v-container>
   </v-toolbar>
