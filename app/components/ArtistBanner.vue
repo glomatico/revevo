57881bf78e3dfb7d2e87d37fd046c35e6e538e24
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 16:17:56 -0300

    Wrap ArtistBanner content in v-card-item

--- app/components/ArtistBanner.vue
+++ app/components/ArtistBanner.vue
@@ -1,18 +1,20 @@
 <template>
   <v-card variant="text">
-    <v-row>
-      <v-col cols="12" sm="auto" class="text-center">
-        <v-avatar size="180">
-          <v-img :src="artist.thumbnail" :alt="`Profile avatar of ${artist.name}.`" />
-        </v-avatar>
-      </v-col>
-      <v-col cols="12" sm="auto" class="align-self-center text-center text-sm-left">
-        <h1>{{ artist.name }}</h1>
-        <h3 class="font-weight-regular">{{ artist.viewCounts.total.toLocaleString() }} views • {{
-          artist.videos.itemsCount.toLocaleString() }}
-          videos</h3>
-      </v-col>
-    </v-row>
+    <v-card-item>
+      <v-row>
+        <v-col cols="12" sm="auto" class="text-center">
+          <v-avatar size="180">
+            <v-img :src="artist.thumbnail" :alt="`Profile avatar of ${artist.name}.`" />
+          </v-avatar>
+        </v-col>
+        <v-col cols="12" sm="auto" class="align-self-center text-center text-sm-left">
+          <h1>{{ artist.name }}</h1>
+          <h3 class="font-weight-regular">{{ artist.viewCounts.total.toLocaleString() }} views • {{
+            artist.videos.itemsCount.toLocaleString() }}
+            videos</h3>
+        </v-col>
+      </v-row>
+    </v-card-item>
   </v-card>
 </template>
 
