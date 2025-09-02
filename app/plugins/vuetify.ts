3b65fb94805d952b85a0c87ba076b835ef40fe67
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-02 16:19:57 -0300

    Set default Vuetify theme to dark

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -5,6 +5,9 @@ import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
+        theme: {
+            defaultTheme: 'dark',
+        }
     })
     app.vueApp.use(vuetify)
 })
