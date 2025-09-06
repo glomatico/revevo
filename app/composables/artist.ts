d12be35fa17764116113b8dccb6327cc2e3c90e6
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-06 13:51:31 -0300

    Remove unused id field from artist video query

--- app/composables/artist.ts
+++ app/composables/artist.ts
@@ -109,8 +109,8 @@ export const useArtist = () => {
           videoData(size: $videosSize, page: $videosPage, sort: $videosSort) {
             videos {
               data {
-                id
                 basicMetaV3 {
+                  isrc
                   title
                   thumbnailUrl
                   duration
