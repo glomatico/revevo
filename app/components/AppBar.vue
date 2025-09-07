853c3dede80c4cc9df9eac42482a67d1b42b598e
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-07 00:11:34 -0300

    Refactor AppBar layout for better responsiveness

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -1,45 +1,48 @@
 <template>
   <v-toolbar color="surface">
     <v-container>
-      <div class="d-none d-md-flex">
-        <v-row align="center">
-          <v-col cols="6">
-            <v-toolbar-title>
-              <NuxtLink class="toolbar-title" to="/">
-                <p class="text-h4 font-weight-bold">Revevo</p>
-              </NuxtLink>
-            </v-toolbar-title>
-          </v-col>
-
-          <v-col>
-            <SearchBar />
-          </v-col>
-        </v-row>
-      </div>
-
-      <div class="d-flex d-md-none">
-        <v-row justify="space-between" align="center">
-          <v-toolbar-title v-if="isLogoVisible">
+      <v-row align="center" class="d-none d-md-flex">
+        <v-col cols="6">
+          <v-toolbar-title>
             <NuxtLink class="toolbar-title" to="/">
               <p class="text-h4 font-weight-bold">Revevo</p>
             </NuxtLink>
           </v-toolbar-title>
+        </v-col>
 
-          <CompactSearchBar @magnify-clicked="isLogoVisible = !isLogoVisible" />
-        </v-row>
-      </div>
+        <v-col>
+          <SearchBar />
+        </v-col>
+      </v-row>
+
+      <v-row align="center" class="d-md-none ma-0">
+        <v-col v-if="isLogoVisible" cols="auto">
+          <v-toolbar-title>
+            <NuxtLink class="toolbar-title" to="/">
+              <p class="text-h4 font-weight-bold">Revevo</p>
+            </NuxtLink>
+          </v-toolbar-title>
+        </v-col>
+
+        <v-col class="text-end">
+          <CompactSearchBar @magnify-clicked="toggleLogo" />
+        </v-col>
+      </v-row>
     </v-container>
   </v-toolbar>
 </template>
 
 <script lang="ts" setup>
 const isLogoVisible = ref<boolean>(true);
+
+const toggleLogo = (isSearchOpen: boolean) => {
+  isLogoVisible.value = !isSearchOpen;
+};
 </script>
 
 <style scoped>
 .toolbar-title {
   all: unset;
   cursor: pointer;
-  display: inline;
 }
 </style>
