ccce5fa5de0187cbf9b03ac4a98d72b4ec2f01e1
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-06 22:38:16 -0300

    Refactor related artists to use ArtistThumbnail component

--- app/components/ArtistPageRelatedArtists.vue
+++ app/components/ArtistPageRelatedArtists.vue
@@ -1,17 +1,7 @@
 <template>
   <v-row>
     <v-col v-if="relatedArtists.length !== 0" v-for="artist in relatedArtists" cols="12" sm="6" md="4" lg="3">
-      <v-card :link="true" hover :to="`/artist/${artist.urlSafeName}`">
-        <v-card-item align="center">
-          <v-avatar size="50%">
-            <v-img :src="artist.thumbnailUrl" :alt="`Profile avatar for ${artist.name}`" :aspect-ratio="1" />
-          </v-avatar>
-        </v-card-item>
-
-        <v-card-title class="text-truncate text-center">
-          {{ artist.name }}
-        </v-card-title>
-      </v-card>
+      <ArtistThumbnail :artist="artist" />
     </v-col>
     <v-col v-else cols="12">
       <v-alert type="info">No related artists found.</v-alert>
---
+++ app/components/ArtistThumbnail.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card :link="true" hover :to="`/artist/${artist.urlSafeName}`">
+    <v-card-item align="center">
+      <v-avatar size="50%">
+        <v-img :src="artist.thumbnailUrl" :alt="`Profile avatar for ${artist.name}`" :aspect-ratio="1" />
+      </v-avatar>
+    </v-card-item>
+
+    <v-card-title class="text-truncate text-center">
+      {{ artist.name }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  artist: BasicArtistMeta;
+}>();
+</script>
