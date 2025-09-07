339ba569b82a9a14c23957c0bfabbdb42709ea6a
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-06 22:37:22 -0300

    Refactor and update homepage layout and copy

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,25 +1,40 @@
 <template>
   <v-container>
-    <h1>
-      Welcome!
-    </h1>
-    <p>
-      Welcome to Revevo - a music video streaming website that recreates the old Vevo experience using their
-      still-active internal APIs.
-    </p>
-    <p>
-      Although the official Vevo website shut down years ago, many parts of its backend remain online. Revevo bridges
-      that gap by using the APIs from the Vevo TV app to reconstruct the original platform, featuring music videos,
-      curated playlists, and artist pages. All rebuilt for both desktop and mobile browsers.
-    </p>
-    <p>
-      Go ahead and start searching for your favorite videos using the search bar above!
-    </p>
-    <v-divider thickness="2" class="my-2" />
-    <p>
-      This project is open source, and you can find the code on
-      <a href="https://github.com/glomatico/revevo" target="_blank">GitHub</a>.
-    </p>
+    <v-row>
+      <v-col cols="12">
+        <p class="text-h4 font-weight-bold">
+          Welcome!
+        </p>
+      </v-col>
+
+      <v-divider thickness="2" class="my-2" />
+
+      <v-col cols="12">
+        <p>
+          Welcome to Revevo - a music video streaming website that recreates the old Vevo experience using their
+          still-active internal APIs.
+        </p>
+      </v-col>
+      <v-col cols="12">
+        <p>
+          Although the official Vevo website shut down years ago, many parts of its backend remain online. Revevo
+          bridges
+          the gap by using those APIs to revive the classic Vevo experience, offering a vast library of high-quality
+          music videos enriched with detailed metadata.
+        </p>
+      </v-col>
+      <v-col cols="12">
+        <p>
+          Go ahead and start searching for your favorite artists and videos using the search bar above!
+        </p>
+      </v-col>
+      <v-col cols="12">
+        <p>
+          This project is open source, and you can find the code on
+          <a href="https://github.com/glomatico/revevo" target="_blank">GitHub</a>.
+        </p>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
