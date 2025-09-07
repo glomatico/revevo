3ed233fd848ea263d924d42ddf7069ccffa9b5be
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-06 23:37:57 -0300

    Add responsive CompactSearchBar to AppBar

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -1,23 +1,39 @@
 <template>
   <v-toolbar color="surface">
     <v-container>
-      <v-row align="center">
-        <v-col cols="8">
-          <v-toolbar-title>
+      <div class="d-none d-md-flex">
+        <v-row align="center">
+          <v-col cols="6">
+            <v-toolbar-title>
+              <NuxtLink class="toolbar-title" to="/">
+                <p class="text-h4 font-weight-bold">Revevo</p>
+              </NuxtLink>
+            </v-toolbar-title>
+          </v-col>
+
+          <v-col>
+            <SearchBar />
+          </v-col>
+        </v-row>
+      </div>
+
+      <div class="d-flex d-md-none">
+        <v-row justify="space-between" align="center">
+          <v-toolbar-title v-if="isLogoVisible">
             <NuxtLink class="toolbar-title" to="/">
               <p class="text-h4 font-weight-bold">Revevo</p>
             </NuxtLink>
           </v-toolbar-title>
-        </v-col>
-        <v-col>
-          <SearchBar />
-        </v-col>
-      </v-row>
+
+          <CompactSearchBar @magnify-clicked="isLogoVisible = !isLogoVisible" />
+        </v-row>
+      </div>
     </v-container>
   </v-toolbar>
 </template>
 
 <script lang="ts" setup>
+const isLogoVisible = ref<boolean>(true);
 </script>
 
 <style scoped>
---
+++ app/components/CompactSearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-btn icon variant="text" @click="handleMagnifyClick" v-if="!isSearchOpen">
+    <v-icon>mdi-magnify</v-icon>
+  </v-btn>
+
+  <div v-if="isSearchOpen" class="d-flex w-100">
+    <v-btn icon variant="text" @click="handleMagnifyClick">
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+    <v-text-field label="Search" variant="solo" density="compact" prepend-inner-icon="mdi-magnify" outlined dense
+      hide-details @keyup.enter="search()" v-model="query">
+      <template #append-inner>
+        <v-btn icon variant="text" @click="search">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+      </template>
+    </v-text-field>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits<{
+  magnifyClicked: [isOpen: boolean]
+}>()
+
+const router = useRouter();
+
+const query = ref<string>('');
+const isSearchOpen = ref<boolean>(false);
+
+const handleMagnifyClick = () => {
+  isSearchOpen.value = !isSearchOpen.value;
+  emit('magnifyClicked', isSearchOpen.value);
+};
+
+const search = async () => {
+  if (!query.value.trim()) return;
+  await router.push({ path: '/search', query: { q: query.value } });
+};
+</script>
