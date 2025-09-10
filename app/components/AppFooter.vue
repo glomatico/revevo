eb0e99869070a14187ac4c4416959aaece0e656d
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-10 09:08:24 -0300

    Update footer with Vevo affiliation disclaimer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <v-footer class="app-footer" color="surface">
     <v-container>
       <v-row justify="space-between">
-        <p>Made by Glomatico.</p>
+        <p>Made by Glomatico. This project is not affiliated with or endorsed by Vevo.</p>
         <v-btn href="https://github.com/glomatico/revevo" target="_blank" icon="mdi-github" class="mx-4" size="medium"
           variant="plain" />
       </v-row>
