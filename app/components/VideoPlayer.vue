8f9aeeca59120d7e8a557ac5ec31ac2b813ef300
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-12 20:09:52 -0300

    Refactor VideoPlayer to use new videoPlayer composable

--- app/components/VideoPlayer.vue
+++ app/components/VideoPlayer.vue
@@ -1,46 +1,38 @@
-<template>
-  <video ref="videoHtml" controls class="video-player">
-  </video>
-</template>
-
 <script lang="ts" setup>
 const props = defineProps<{
-  streamUrl?: string;
   captionsUrl?: string;
-  streamType: StreamType;
+  streamUrl?: string;
 }>();
 
-const { attachHlsVideo, attachNormalVideo, attachCaptions } = useVideo();
+const { loadSettings, settings } = useSettings();
 
-const videoHtml = ref<HTMLVideoElement>();
-
-const loadVideo = async () => {
-  try {
-    if (props.streamType === StreamType.HLS) {
-      await attachHlsVideo(videoHtml.value!, props.streamUrl);
-    }
-    if (props.streamType === StreamType.MP4) {
-      await attachNormalVideo(videoHtml.value!, props.streamUrl);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  await loadCaptions();
-};
-
-const loadCaptions = async () => {
-  try {
-    await attachCaptions(videoHtml.value!, props.captionsUrl);
-  } catch (error) {
-    console.error(error);
-  }
-};
+const {
+  loadVideoPlayer,
+  streamUrl,
+  captionsUrl,
+  htmlVideo
+} = useVideoPlayer(settings.value);
 
 watch(props, async () => {
-  await loadVideo();
-});
+  if (!props.streamUrl) {
+    return;
+  }
 
+  streamUrl.value = props.streamUrl;
+  captionsUrl.value = props.captionsUrl;
+
+  await loadVideoPlayer();
+})
+
+onMounted(async () => {
+  loadSettings();
+});
 </script>
+
+<template>
+  <video ref="htmlVideo" controls class="video-player">
+  </video>
+</template>
 
 <style scoped>
 .video-player {
---
+++ app/composables/videoPlayer.ts
@@ -0,0 +1,92 @@
+import Hls from 'hls.js';
+import { PlaybackMethod } from '.';
+
+export const useVideoPlayer = (settings: Settings | null) => {
+  const htmlVideo = ref<HTMLVideoElement>();
+  const streamUrl = ref<string | null>();
+  const captionsUrl = ref<string | null>();
+
+  const addCaptionsEventListener = () => {
+    if (!settings) return;
+
+    htmlVideo.value!.textTracks.addEventListener('change', () => {
+      const tracks = htmlVideo.value?.textTracks;
+      if (!tracks) return;
+
+      Array.from(tracks).forEach((track) => {
+        if (track.kind !== 'subtitles') return;
+        settings.enableCaptions = track.mode === 'showing';
+      });
+    });
+  };
+
+  const attachCaptions = async () => {
+    if (!captionsUrl.value) return;
+
+    const response = await fetch(captionsUrl.value);
+    if (!response.ok) return;
+
+    const blob = new Blob([await response.text()], { type: 'text/vtt' });
+    const url = URL.createObjectURL(blob);
+
+    const track = document.createElement('track') as HTMLTrackElement;
+
+    track.kind = 'subtitles';
+    track.label = 'Unknown Language';
+    track.src = url;
+    track.default = false;
+
+    htmlVideo.value!.appendChild(track);
+
+    toggleCaptionsFromStorage();
+  };
+
+  const toggleCaptionsFromStorage = async () => {
+    Array.from(htmlVideo.value!.textTracks).forEach((track) => {
+      if (track.kind === 'subtitles') {
+        track.mode = settings?.enableCaptions! ? 'showing' : 'hidden';
+      }
+    });
+  };
+
+  const attachHlsVideo = async () => {
+    if (!htmlVideo.value) return;
+
+    const hls = new Hls();
+    hls.loadSource(streamUrl.value!);
+    hls.attachMedia(htmlVideo.value);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      htmlVideo.value?.play();
+    });
+  };
+
+  const attachNormalVideo = async () => {
+    if (!htmlVideo.value) return;
+
+    htmlVideo.value.src = streamUrl.value!;
+    htmlVideo.value.play();
+  };
+
+  const attachVideo = async () => {
+    if (settings?.playbackMethod === PlaybackMethod.MP4) {
+      await attachNormalVideo();
+    } else {
+      await attachHlsVideo();
+    }
+  };
+
+  const loadVideoPlayer = async (
+  ) => {
+    await attachVideo();
+    await attachCaptions();
+
+    addCaptionsEventListener();
+  };
+
+  return {
+    loadVideoPlayer,
+    streamUrl,
+    captionsUrl,
+    htmlVideo,
+  };
+}
