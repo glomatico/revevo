ecde17ed0a8f82ef4a1d638bc5ed668960035692
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-13 18:09:16 -0300

    Update token cookie type in artist composable

--- app/composables/artist.ts
+++ app/composables/artist.ts
@@ -86,7 +86,7 @@ export const useArtist = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${useCookie<string | null>('token').value}`,
+        'Authorization': `Bearer ${useCookie<string>('token').value}`,
       },
       body: JSON.stringify({
         query,
@@ -152,7 +152,7 @@ export const useArtist = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${useCookie<string | null>('token').value}`,
+        'Authorization': `Bearer ${useCookie<string>('token').value}`,
       },
       body: JSON.stringify({
         query,
