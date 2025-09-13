55824f24bcbf644322b34494b79716840ab71201
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-13 00:14:08 -0300

    Refactor home page data loading and section logic

--- app/composables/homePage.ts
+++ app/composables/homePage.ts
@@ -2,6 +2,13 @@ export const useHomePage = () => {
   const config = useRuntimeConfig();
   const graphqlApiUrl = config.public.graphqlApiUrl;
 
+  const loadingState = ref<LoadingState>(LoadingState.LOADING);
+  const homePage = ref<HomePage | null>();
+  const topVideosSection = ref<HomePageContainer | null>();
+  const trendingArtistsSection = ref<HomePageContainer | null>();
+  const filteredTopVideosSectionItems = ref<Video[]>();
+  const filteredTrendingArtistsSectionItems = ref<Artist[]>();
+
   const getHomePage = async (
     offset: number = 0,
     limit: number = 20,
@@ -109,7 +116,42 @@ export const useHomePage = () => {
     return homePageRespons.data.homePage;
   };
 
+  const fetchHomePage = async () => {
+    try {
+      homePage.value = await getHomePage();
+    } catch (error) {
+      console.error(error);
+      loadingState.value = LoadingState.ERROR;
+    }
+    loadingState.value = LoadingState.LOADED;
+  };
+
+  const loadHomePageVideoSection = () => {
+    topVideosSection.value = homePage.value!.containersV2.find(s => s.serviceName === 'top-videos') || null;
+
+    filteredTopVideosSectionItems.value = topVideosSection.value!.items.map(i => i.item?.video!).filter(v => isVideoValid(v, false));
+  };
+
+  const loadHomePageArtistSection = () => {
+    trendingArtistsSection.value = homePage.value!.containersV2.find(s => s.serviceName === 'trending-artists') || null;
+
+    filteredTrendingArtistsSectionItems.value = trendingArtistsSection.value!.items.map(i => i.item?.artist!).filter(a => isArtistValid(a));
+  };
+
+
+  const loadHomePage = async () => {
+    await fetchHomePage();
+    loadHomePageVideoSection();
+    loadHomePageArtistSection();
+  };
+
   return {
-    getHomePage,
+    loadHomePage,
+    loadingState,
+    homePage,
+    topVideosSection,
+    trendingArtistsSection,
+    filteredTopVideosSectionItems,
+    filteredTrendingArtistsSectionItems,
   };
 }
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+
   <v-container>
     <v-row>
       <v-col v-if="loadingState === LoadingState.LOADING" cols="12">
@@ -10,7 +11,7 @@
       <template v-else>
         <v-col cols="12">
           <p class="text-h4">
-            {{ topVideosSectionTitle }}
+            {{ topVideosSection?.title }}
           </p>
         </v-col>
 
@@ -18,7 +19,7 @@
 
         <v-col cols="12">
           <v-row>
-            <v-col v-for="(video, index) in topVideosSectionItems" :key="index" cols="12" sm="6" md="4" lg="3">
+            <v-col v-for="(video, index) in filteredTopVideosSectionItems" :key="index" cols="12" sm="6" md="4" lg="3">
               <VideoThumbnail v-if="video" :video="video" :vertical="true" />
             </v-col>
           </v-row>
@@ -26,7 +27,7 @@
 
         <v-col cols="12">
           <p class="text-h4">
-            {{ trendingArtistsSectionTitle }}
+            {{ trendingArtistsSection?.title }}
           </p>
         </v-col>
 
@@ -34,7 +35,7 @@
 
         <v-col cols="12">
           <v-slide-group>
-            <v-slide-group-item v-for="(artist, index) in trendingArtistsSectionItems" :key="index">
+            <v-slide-group-item v-for="(artist, index) in filteredTrendingArtistsSectionItems" :key="index">
               <div class="ma-2">
                 <ArtistThumbnail :artist="artist.basicMeta" class="artist-thumbnail" />
               </div>
@@ -47,49 +48,24 @@
 </template>
 
 <script lang="ts" setup>
-const { getHomePage } = useHomePage();
-const { isArtistValid } = useArtist();
-
-const loadingState = ref<LoadingState>(LoadingState.LOADING);
-const homePage = ref<HomePage | null>(null);
-const topVideosSectionTitle = ref<string | null>(null);
-const topVideosSectionItems = ref<Video[]>([]);
-const trendingArtistsSectionTitle = ref<string | null>(null);
-const trendingArtistsSectionItems = ref<Artist[]>([]);
-
-const loadHomePage = async () => {
-  try {
-    homePage.value = await getHomePage();
-  } catch (error) {
-    console.error(error);
-    loadingState.value = LoadingState.ERROR;
-  }
-  loadingState.value = LoadingState.LOADED;
-};
-
-const setSections = async () => {
-  if (!homePage.value) return;
-
-  const topVideosSection = homePage.value.containersV2.find(s => s.serviceName === 'top-videos') || null;
-  topVideosSectionTitle.value = topVideosSection?.title || null;
-  topVideosSectionItems.value = topVideosSection?.items.map(i => i.item?.video!).filter(v => isVideoValid(v, false)) || [];
-
-
-  const trendingArtistsSection = homePage.value.containersV2.find(s => s.serviceName === 'trending-artists') || null;
-  trendingArtistsSectionTitle.value = trendingArtistsSection?.title || null;
-  trendingArtistsSectionItems.value = trendingArtistsSection?.items.map(i => i.item?.artist!).filter(a => isArtistValid(a)) || [];
-};
+const {
+  loadHomePage,
+  loadingState,
+  topVideosSection,
+  trendingArtistsSection,
+  filteredTopVideosSectionItems,
+  filteredTrendingArtistsSectionItems,
+} = useHomePage();
 
 onMounted(async () => {
   await loadHomePage();
-  await setSections();
 });
 
 useSeoMeta({
   title: 'Revevo',
   ogTitle: 'Revevo',
-  description: 'A music video streaming website that recreates the old Vevo experience using their still-active internal APIs.',
-  ogDescription: 'A music video streaming website that recreates the old Vevo experience using their still-active internal APIs.',
+  description: 'A music video streaming website that revives the old Vevo experience.',
+  ogDescription: 'A music video streaming website that revives the old Vevo experience.',
   ogImage: 'https://img.vevo.com/images/defaultbackstageassets/vevo.png?width=720&height=720',
   twitterCard: 'summary_large_image',
 })
