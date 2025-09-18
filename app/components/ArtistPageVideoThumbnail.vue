057554b4288146d9eb2711b807c9e1f1c22823b5
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-18 12:13:03 -0300

    Refactor ArtistPageVideoThumbnail layout

--- app/components/ArtistPageVideoThumbnail.vue
+++ app/components/ArtistPageVideoThumbnail.vue
@@ -1,23 +1,3 @@
-<template>
-  <v-card :link="true" hover :to="`/video?v=${video.basicMetaV3.isrc}`">
-    <v-img :src="video.basicMetaV3.thumbnailUrl" :alt="`Thumbnail for ${video.basicMetaV3.title}`" cover
-      :aspect-ratio="16 / 9" />
-    <v-card-item>
-      <v-card-title class="text-truncate text-center" :title="video.basicMetaV3.title">
-        <v-icon v-if="isNewRelease" size="24" icon="mdi-new-box" />
-        <v-icon v-if="video.basicMetaV3.explicit" size="24" icon="mdi-alpha-e-box" />
-        {{ video.basicMetaV3.title }}
-      </v-card-title>
-      <v-card-subtitle class="text-center">
-        {{ formatDuration(video.basicMetaV3.duration!) }}
-        <template v-if="video.views?.viewsTotal">
-          • {{ video.views?.viewsTotal.toLocaleString() }} views
-        </template>
-      </v-card-subtitle>
-    </v-card-item>
-  </v-card>
-</template>
-
 <script lang="ts" setup>
 const props = defineProps<{
   video: Video;
@@ -26,7 +6,35 @@ const props = defineProps<{
 const videoReleaseDate = props.video.basicMetaV3.releaseDate
   ? new Date(props.video.basicMetaV3.releaseDate)
   : null;
-const isNewRelease = ref<boolean>(videoReleaseDate
+const isNewRelease = ref(videoReleaseDate
   ? (new Date().getTime() - videoReleaseDate.getTime()) / (1000 * 60 * 60 * 24) <= 14
   : false);
 </script>
+
+<template>
+  <v-card :to="`/video?v=${video.basicMetaV3.isrc}`">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-img :src="video.basicMetaV3.thumbnailUrl" :alt="`Thumbnail for ${video.basicMetaV3.title}`" cover
+          :aspect-ratio="16 / 9" />
+      </v-col>
+
+      <v-col cols="12">
+        <v-card-item>
+          <v-card-title class="text-truncate text-center" :title="video.basicMetaV3.title">
+            <v-icon v-if="isNewRelease" icon="mdi-new-box" />
+            <v-icon v-if="video.basicMetaV3.explicit" icon="mdi-alpha-e-box" />
+            {{ video.basicMetaV3.title }}
+          </v-card-title>
+
+          <v-card-subtitle class="text-center">
+            {{ formatDuration(video.basicMetaV3.duration!) }}
+            <template v-if="video.views?.viewsTotal">
+              • {{ video.views?.viewsTotal.toLocaleString() }} views
+            </template>
+          </v-card-subtitle>
+        </v-card-item>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
