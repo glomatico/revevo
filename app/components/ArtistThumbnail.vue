52bc390aa48f454a6b69adb1f800ebd7ded813f9
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-18 16:11:13 -0300

    Refactor ArtistThumbnail avatar rendering

--- app/components/ArtistThumbnail.vue
+++ app/components/ArtistThumbnail.vue
@@ -1,29 +1,34 @@
-<template>
-  <v-card :to="`/artist/${artist.urlSafeName}`">
-    <v-row no-gutters>
-      <v-col cols="12" align="center">
-        <v-avatar size="180" class="ma-2">
-          <v-img v-if="resizedAvatarlUrl" :src="resizedAvatarlUrl" :alt="`Profile avatar of ${artist.name}.`" />
-          <v-icon v-else size="120">mdi-account-circle</v-icon>
-        </v-avatar>
-      </v-col>
-      <v-col cols="12">
-        <v-card-item>
-          <p class="text-center">{{ artist.name }}</p>
-        </v-card-item>
-      </v-col>
-    </v-row>
-  </v-card>
-</template>
-
 <script lang="ts" setup>
 const props = defineProps<{
   artist: BasicArtistMeta;
 }>();
 
-const resizedAvatarlUrl = ref<string | null>(null);
+const resizedAvatarlUrl = ref('');
 
 if (props.artist?.thumbnailUrl) {
   resizedAvatarlUrl.value = resizeImageUrl(props.artist.thumbnailUrl, 256, 256);
 }
 </script>
+
+<template>
+  <v-card :to="`/artist/${artist.urlSafeName}`">
+    <v-row no-gutters>
+      <v-col cols="12" align="center">
+        <v-img class="ma-2" :src="resizedAvatarlUrl" :alt="`Profile avatar of ${artist.name}.`" rounded="circle"
+          :aspect-ratio="1" width="120">
+          <div v-if="!resizedAvatarlUrl">
+            <v-icon size="120">
+              mdi-account-circle
+            </v-icon>
+          </div>
+        </v-img>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card-item>
+          <p class="text-center text-truncate">{{ artist.name }}</p>
+        </v-card-item>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
