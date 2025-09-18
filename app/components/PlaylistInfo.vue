589d7e70d5b376687692e8cb2e3e08788860b83d
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-18 16:11:09 -0300

    Refactor PlaylistInfo layout and styling

--- app/components/PlaylistInfo.vue
+++ app/components/PlaylistInfo.vue
@@ -1,23 +1,23 @@
-<template>
-  <v-card variant="text">
-    <v-img :src="playlist.basicMeta.image_url" :aspect-ratio="16 / 9" />
-    <v-card-item class="text-center">
-      <v-card-title>{{ playlist.basicMeta.title }}</v-card-title>
-      <v-card-text v-if="playlist.basicMeta.description">
-        {{ playlist.basicMeta.description }}
-      </v-card-text>
-      <v-card-subtitle>
-        <template v-if="playlist.basicMeta.curated">
-          Curated Playlist •
-        </template>
-        {{ playlist.basicMeta.videoCount }} video{{ playlist.basicMeta.videoCount !== 1 ? 's' : '' }}
-      </v-card-subtitle>
-    </v-card-item>
-  </v-card>
-</template>
-
 <script lang="ts" setup>
 defineProps<{
   playlist: Playlist;
 }>();
 </script>
+
+<template>
+  <v-card variant="text">
+    <v-img :src="playlist.basicMeta.image_url" :aspect-ratio="16 / 9" />
+    <v-card-item class="text-center">
+      <p class="text-h4 font-weight-bold">{{ playlist.basicMeta.title }}</p>
+      <p v-if="playlist.basicMeta.description">
+        {{ playlist.basicMeta.description }}
+      </p>
+      <p>
+        <template v-if="playlist.basicMeta.curated">
+          Curated Playlist •
+        </template>
+        {{ playlist.basicMeta.videoCount }} video{{ playlist.basicMeta.videoCount !== 1 ? 's' : '' }}
+      </p>
+    </v-card-item>
+  </v-card>
+</template>
