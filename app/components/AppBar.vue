642989549a244eacf8137b5520e1b92efaa2b2dd
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-09-18 11:15:18 -0300

    Refactor AppBar into compact and normal variants

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -1,51 +1,13 @@
 <script lang="ts" setup>
-const appMenu = ref();
+
 </script>
 
 <template>
-  <AppMenu ref="appMenu" />
+  <div class="d-none d-sm-block align-center">
+    <AppBarNormal />
+  </div>
 
-  <v-toolbar color="surface">
-    <v-container>
-      <div class="d-none d-sm-block align-center">
-        <v-row align="center">
-          <v-col cols="auto">
-            <v-app-bar-nav-icon @click=" appMenu.toggleDrawer()" />
-          </v-col>
-
-          <v-col cols="auto">
-            <v-toolbar-title>
-              <NuxtLink class="text-decoration-none text-white" to="/">
-                Revevo
-              </NuxtLink>
-            </v-toolbar-title>
-          </v-col>
-
-          <v-col align="center">
-            <SearchBar />
-          </v-col>
-        </v-row>
-      </div>
-
-      <div class="d-sm-none align-center">
-        <v-row align="center">
-          <v-col cols="auto">
-            <v-app-bar-nav-icon @click=" appMenu.toggleDrawer()" />
-          </v-col>
-
-          <v-col cols="auto">
-            <v-toolbar-title>
-              <NuxtLink class="text-decoration-none text-white" to="/">
-                Revevo
-              </NuxtLink>
-            </v-toolbar-title>
-          </v-col>
-
-          <v-col align="right">
-            <CompactSearchBar />
-          </v-col>
-        </v-row>
-      </div>
-    </v-container>
-  </v-toolbar>
+  <div class="d-sm-none align-center">
+    <AppBarCompact />
+  </div>
 </template>
---
+++ app/components/AppBarNormal.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+const router = useRouter();
+const route = useRoute();
+
+const appMenu = ref();
+const query = ref<string>(route.query.q as string || '');
+
+const search = async () => {
+  if (!query.value.trim()) return;
+
+  await router.push({
+    path: '/search',
+    query: {
+      q: query.value,
+    }
+  });
+};
+</script>
+
+<template>
+  <AppMenu ref="appMenu" />
+
+  <v-toolbar color="surface">
+    <v-container>
+      <v-row align="center">
+        <v-col cols="auto">
+          <v-app-bar-nav-icon @click="appMenu.toggleDrawer()" />
+        </v-col>
+
+        <v-col cols="auto">
+          <v-toolbar-title>
+            <NuxtLink class="text-decoration-none text-white" to="/">
+              Revevo
+            </NuxtLink>
+          </v-toolbar-title>
+        </v-col>
+
+        <v-col align="center">
+          <v-text-field rounded label="Search" variant="solo" single-line density="compact" outlined dense hide-details
+            @keyup.enter="search" v-model="query" max-width="600">
+            <template #append-inner>
+              <v-icon-btn variant="text" @click="search" icon="mdi-send">
+              </v-icon-btn>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-toolbar>
+</template>
