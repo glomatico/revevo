d187a6aca1c47fa12e64759b1b3f2735600f9c83
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-20 17:40:14 -0300

    Refactor ArtistLinkChip to accept single artist props

--- app/components/ArtistLinkChip.vue
+++ app/components/ArtistLinkChip.vue
@@ -1,41 +1,27 @@
-<template>
-  <v-row dense>
-    <v-col v-for="artist in artistsFiltered" :key="artist.basicMeta.name" cols="auto">
-      <v-chip rounded :to="`/artist/${artist.basicMeta.urlSafeName}`">
-        <v-row dense align="center">
-          <v-col>
-            <v-avatar v-if="addAvatar">
-              <v-img v-if="artist.basicMeta.thumbnailUrl" :src="resizeImageUrl(artist.basicMeta.thumbnailUrl, 32, 32)"
-                :alt="`Avatar for ${artist.basicMeta.name}`" />
-              <v-icon v-else size="26">mdi-account-circle</v-icon>
-            </v-avatar>
-          </v-col>
-
-          <v-col>
-            <p>
-              {{ artist.basicMeta.name }}
-            </p>
-          </v-col>
-        </v-row>
-      </v-chip>
-    </v-col>
-  </v-row>
-</template>
-
 <script lang="ts" setup>
-const props = defineProps<{
-  videoArtists: Artist[];
-  mainOnly?: boolean;
-  addAvatar?: boolean;
+defineProps<{
+  artistId: string;
+  artistName: string;
+  artistThumbnail: string;
 }>();
-
-const artistsFiltered = ref<Artist[]>([]);
-
-onMounted(async () => {
-  if (props.mainOnly) {
-    artistsFiltered.value = props.videoArtists.filter(artist => artist.basicMeta.role === 'main');
-  } else {
-    artistsFiltered.value = props.videoArtists;
-  }
-});
 </script>
+
+
+<template>
+  <v-chip rounded :to="`/artist/${artistId}`">
+    <v-row dense align="center">
+      <v-col>
+        <v-avatar>
+          <v-img v-if="artistThumbnail" :src="artistThumbnail" :alt="`Profile avatar for ${artistName}`" />
+          <v-icon v-else size="26">mdi-account-circle</v-icon>
+        </v-avatar>
+      </v-col>
+
+      <v-col>
+        <p>
+          {{ artistName }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-chip>
+</template>
