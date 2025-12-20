c023cd0a83749bbc8f45beafd7df56fb97ff326e
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-20 17:40:23 -0300

    Refactor VideoInfo to use props and slots

--- app/components/VideoInfo.vue
+++ app/components/VideoInfo.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col cols="12">
       <p class="text-h4 text-wrap text-break">
-        {{ video.basicMetaV3.title }}
+        {{ title }}
       </p>
     </v-col>
 
@@ -11,91 +11,76 @@
     <v-col cols="12">
       <v-table class="rounded-lg">
         <tbody>
-          <tr>
+          <tr v-if="$slots.artists">
             <td>Artists</td>
             <td>
-              <ArtistLinkChip :video-artists="video.basicMetaV3.artists!" add-avatar use-chips />
+              <slot name="artists" />
             </td>
           </tr>
-          <tr v-if="video.views?.viewsTotal">
+          <tr v-if="views">
             <td>Views</td>
-            <td>{{ video.views.viewsTotal.toLocaleString() }}</td>
-          </tr>
-          <tr v-if="video.basicMetaV3.releaseDate">
-            <td>Release date</td>
-            <td>{{ new Date(video.basicMetaV3.releaseDate!).toLocaleString() }}</td>
+            <td>{{ views.toLocaleString() }}</td>
           </tr>
           <tr>
-            <td>Genres</td>
-            <td>
-              {{ video.basicMetaV3.genres!.join(', ') || 'N/A' }}
-            </td>
+            <td>ISRC</td>
+            <td>{{ id }}</td>
           </tr>
-          <tr v-for="(item, index) in groupCreditsByRole(video.basicMetaV3.credits!)" :key="index">
-            <td>{{ item.role }}</td>
+          <tr>
+            <td>Duration</td>
+            <td>{{ formatDuration(duration) }}</td>
+          </tr>
+          <tr v-if="date">
+            <td>Release date</td>
+            <td>{{ new Date(date).toLocaleString() }}</td>
+          </tr>
+          <tr>
+            <td>Genre</td>
             <td>
-              {{ item.name }}
+              {{ genre }}
             </td>
           </tr>
           <tr>
             <td>Explicit</td>
-            <td>{{ video.basicMetaV3.explicit ? 'Yes' : 'No' }}</td>
-          </tr>
-          <tr v-if="video.views?.youTubeId">
-            <td>YouTube URL</td>
-            <td>
-              <a :href="`https://www.youtube.com/watch?v=${video.views.youTubeId}`" target="_blank"
-                rel="noopener noreferrer">
-                https://www.youtube.com/watch?v={{ video.views.youTubeId }}
-              </a>
-            </td>
+            <td>{{ explicit ? 'Yes' : 'No' }}</td>
           </tr>
           <tr>
+            <td>Lyric Video</td>
+            <td>{{ lyricVideo ? 'Yes' : 'No' }}</td>
+          </tr>
+          <tr v-if="copyright">
             <td>Copyright</td>
-            <td>{{ video.basicMetaV3.copyright }}</td>
+            <td>{{ copyright }}</td>
+          </tr>
+          <tr v-if="copyrightYear">
+            <td>Copyright Year</td>
+            <td>{{ copyrightYear }}</td>
           </tr>
           <tr>
-            <td>Duration</td>
-            <td>{{ formatDuration(video.basicMetaV3.duration!) }}</td>
-          </tr>
-          <tr>
-            <td>ISRC</td>
-            <td>{{ video.basicMetaV3.isrc }}</td>
+            <td>Label</td>
+            <td>{{ label }}</td>
           </tr>
         </tbody>
       </v-table>
     </v-col>
-    <v-col cols="12">
-      <VideoStreamUrls :video="video" />
+
+    <v-col v-if="$slots.streamurls" cols="12">
+      <slot name="streamurls" />
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts" setup>
-defineProps<{
-  video: Video;
+const props = defineProps<{
+  id: string;
+  title: string;
+  genre: string;
+  duration: number;
+  explicit: boolean;
+  lyricVideo: boolean;
+  date: string;
+  copyright: string;
+  label: string;
+  copyrightYear: number;
+  views: number;
 }>();
-
-const groupCreditsByRole = (credits: CreditsV3[]): { role: string; name: string }[] => {
-  if (!credits?.length) {
-    return [];
-  }
-
-  const roleMap = new Map<string, string[]>();
-
-  credits.forEach((credit) => {
-    if (!credit?.role || !credit?.name) return;
-
-    const existingNames = roleMap.get(credit.role) ?? [];
-    roleMap.set(credit.role, [...existingNames, credit.name]);
-  });
-
-
-  return Array.from(roleMap.entries())
-    .map(([role, names]) => ({
-      role,
-      name: names.join(', ')
-    }))
-    .sort((a, b) => a.role.localeCompare(b.role));
-};
 </script>
