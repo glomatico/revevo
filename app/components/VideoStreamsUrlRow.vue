09bdd1d57babd8a931c6be29849efae652ff0642
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-20 17:40:30 -0300

    Add VideoStreamsUrlRow component

---
+++ app/components/VideoStreamsUrlRow.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+  label: string;
+  url: string;
+}>();
+</script>
+
+<template>
+  <tr>
+    <td>
+      {{ label }}
+    </td>
+
+    <td>
+      <CopyableReadOnlyField :value="url" />
+    </td>
+  </tr>
+</template>
