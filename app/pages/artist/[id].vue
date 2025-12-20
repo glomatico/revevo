406ee1eec7bd26b8498716b25804cb28370eea8b
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-20 11:10:37 -0300

    Add missing :key attributes to v-for loops

--- app/pages/artist/[id].vue
+++ app/pages/artist/[id].vue
@@ -59,8 +59,8 @@ onMounted(async () => {
         <v-col cols="12" align="center">
           <v-btn-toggle v-model="sortVideos" @update:model-value="loadAllVideos"
             :disabled="loadingStateVideos === LoadingState.LOADING">
-            <v-btn v-for="option in ['views', 'date', 'a-z']" class="text-none text-capitalize" :value="option"
-              variant="outlined">
+            <v-btn v-for="option in ['views', 'date', 'a-z']" :key="option" class="text-none text-capitalize"
+              :value="option" variant="outlined">
               {{ option }}
             </v-btn>
           </v-btn-toggle>
@@ -83,7 +83,7 @@ onMounted(async () => {
         <v-col v-else cols="12">
           <v-infinite-scroll @load="loadVideos">
             <v-row class="mx-0">
-              <v-col v-for="video in filteredVideos" cols="12" sm="6" md="4" lg="3">
+              <v-col v-for="video in filteredVideos" :key="video" cols="12" sm="6" md="4" lg="3">
                 <VideoThumbnail :id="video.id" :title="video.title" :created="video.created"
                   :thumbnail-url="video.thumbnail" :explicit="video.explicit" :duration="video.duration"
                   :views="video.viewCounts.total" vertical>
