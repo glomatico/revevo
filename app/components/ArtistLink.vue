0d84e0859c5cc47f2d4e1f441421226448e5281d
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-21 18:01:59 -0300

    Refactor ArtistLink to accept single artist props

--- app/components/ArtistLink.vue
+++ app/components/ArtistLink.vue
@@ -1,23 +1,13 @@
 <template>
-  <NuxtLink v-for="(artist, index) in artistsFiltered" :key="artist.basicMeta.name"
-    :to="`/artist/${artist.basicMeta.urlSafeName}`" class="text-decoration-none">
-    {{ artist.basicMeta.name + (index == artistsFiltered.length - 1 ? '' : ',&nbsp;') }}
+  <NuxtLink :to="`/artist/${artistId}`" class="text-decoration-none">
+    {{ artistName + (last ? '' : ',&nbsp;') }}
   </NuxtLink>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
-  videoArtists: Artist[];
-  mainOnly?: boolean;
+defineProps<{
+  artistId: string;
+  artistName: string;
+  last?: boolean;
 }>();
-
-const artistsFiltered = ref<Artist[]>([]);
-
-onMounted(async () => {
-  if (props.mainOnly) {
-    artistsFiltered.value = props.videoArtists.filter(artist => artist.basicMeta.role === 'main');
-  } else {
-    artistsFiltered.value = props.videoArtists;
-  }
-});
 </script>
