69bc2a282984821d868a86e6486f7dffe7d371d8
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-21 17:56:50 -0300

    Refactor VideoStreamUrls dialog and button styles

--- app/components/VideoStreamUrls.vue
+++ app/components/VideoStreamUrls.vue
@@ -1,42 +1,31 @@
-<template>
-  <v-btn block variant="text" class="text-none" @click="isExtraInfoDialogVisible = true">Show Stream URLs</v-btn>
+<script lang="ts" setup>
+const dialogVisible = ref(false);
+</script>
 
-  <v-dialog v-model="isExtraInfoDialogVisible" max-width="600px">
+<template>
+  <v-btn block variant="outlined" class="text-none" @click="dialogVisible = true">Show Stream URLs</v-btn>
+
+  <v-dialog v-model="dialogVisible" max-width="600px">
     <v-card>
       <v-card-item>
         <v-row dense>
           <v-col cols="12">
             <p class="text-h6">Stream URLs</p>
           </v-col cols="12">
+
           <v-col>
-            <v-table fixed-header>
-              <tbody v-for="value in video.streamsV3" :key="value.url">
-                <tr>
-                  <td>{{ value.format }}
-                    <template v-if="value.quality">
-                      ({{ value.quality }})
-                    </template>
-                  </td>
-                  <td>
-                    <CopyableReadOnlyField :value="value.url" />
-                  </td>
-                </tr>
+            <v-table>
+              <tbody>
+                <slot />
               </tbody>
             </v-table>
           </v-col>
+
           <v-col cols="12" align="end">
-            <v-btn variant="text" @click="isExtraInfoDialogVisible = false">Close</v-btn>
+            <v-btn variant="outlined" class="text-none" @click="dialogVisible = false">Close</v-btn>
           </v-col>
         </v-row>
       </v-card-item>
     </v-card>
   </v-dialog>
 </template>
-
-<script lang="ts" setup>
-defineProps<{
-  video: Video;
-}>();
-
-const isExtraInfoDialogVisible = ref(false);
-</script>
