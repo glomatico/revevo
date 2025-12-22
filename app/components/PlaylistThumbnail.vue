d77263a03fae7561da547b97ad68e783b1689b55
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-21 23:13:06 -0300

    Refactor PlaylistThumbnail props for simplicity

--- app/components/PlaylistThumbnail.vue
+++ app/components/PlaylistThumbnail.vue
@@ -1,20 +1,21 @@
 <script lang="ts" setup>
 defineProps<{
-  playlist: Playlist;
+  id: string;
+  thumbnailUrl: string;
+  title: string;
 }>();
 </script>
 
 <template>
-  <v-card :to="`/playlist/${playlist.id}`" min-width="300">
+  <v-card :to="`/playlist/${id}`">
     <v-row no-gutters align="center">
       <v-col cols="12">
-        <v-img :src="playlist.basicMeta.image_url" :alt="`Thumbnail for ${playlist.basicMeta.title}`"
-          :aspect-ratio="16 / 9" cover />
+        <v-img :src="thumbnailUrl" :alt="`Thumbnail for ${title}`" :aspect-ratio="16 / 9" cover />
       </v-col>
 
       <v-col cols="12">
         <v-card-item>
-          <p class="text-center text-truncate">{{ playlist.basicMeta.title }}</p>
+          <p class="text-center text-truncate">{{ title }}</p>
         </v-card-item>
       </v-col>
     </v-row>
