14f557530ec366f3c2de1acccf85a7bc060d3641
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2025-12-21 21:59:11 -0300

    Refactor AppTabs to improve tab handling and layout

--- app/components/AppTabs.vue
+++ app/components/AppTabs.vue
@@ -1,28 +1,20 @@
-<template>
-  <v-tabs v-model="tab" align-tabs="center">
-    <v-tab class="text-none" v-for="tab in tabs" :key="tab" :value="tab">{{ wordfy(tab) }}</v-tab>
-  </v-tabs>
-</template>
-
 <script lang="ts" setup>
 const props = defineProps<{
   tabs: string[],
   routeParamKey: string,
-  defaultTab: string
-}>()
-
-const emit = defineEmits<{
-  tabChange: [value: string]
-}>()
+}>();
 
 const route = useRoute();
 const router = useRouter();
 
-const tab = computed<string>({
-  get: () => (route.query[props.routeParamKey] as string) || props.defaultTab,
+const tab = computed({
+  get: () => {
+    const value = route.query[props.routeParamKey];
+    return typeof value === 'string' ? value : props.tabs[0];
+  },
   set: async (value: string) => {
     const newQuery = { ...route.query };
-    if (value === props.defaultTab) {
+    if (value === props.tabs[0]) {
       delete newQuery[props.routeParamKey];
     } else {
       newQuery[props.routeParamKey] = value;
@@ -34,12 +26,24 @@ const tab = computed<string>({
     });
   }
 });
-
-const wordfy = (value: string) => {
-  return value.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-};
-
-watch(tab, async () => {
-  emit('tabChange', tab.value);
-});
 </script>
+
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-tabs v-model="tab" align-tabs="center">
+        <v-tab class="text-none text-capitalize" v-for="tab in tabs" :key="tab" :value="tab">{{ tab }}</v-tab>
+      </v-tabs>
+
+      <v-divider thickness="2" />
+    </v-col>
+
+    <v-col cols="12">
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item v-for="tabName in tabs" :key="tabName" :value="tabName">
+          <slot :name="tabName" />
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-col>
+  </v-row>
+</template>
