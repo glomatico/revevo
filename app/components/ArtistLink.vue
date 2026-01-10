f2d9404eb15565de3a4a460b051872fb6d44e037
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-01-10 15:44:41 -0300

    Refactor ArtistLink to support multiple artists

--- app/components/ArtistLink.vue
+++ app/components/ArtistLink.vue
@@ -1,13 +1,13 @@
-<template>
-  <NuxtLink :to="`/artist/${artistId}`" class="text-decoration-none">
-    {{ artistName + (last ? '' : ',&nbsp;') }}
-  </NuxtLink>
-</template>
-
 <script lang="ts" setup>
 defineProps<{
-  artistId: string;
-  artistName: string;
-  last?: boolean;
+  artists: any[];
 }>();
 </script>
+
+<template>
+  <template v-for="(artist, index) in artists">
+    <NuxtLink :to="`/artist/${artist.artist.id}`" class="text-decoration-none">
+      {{ artist.artist.name + (index === artists.length - 1 ? '' : ',&nbsp;') }}
+    </NuxtLink>
+  </template>
+</template>
