c82a341ce087f335af262b9a42df1477fd67813e
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-01-17 16:58:47 -0300

    Refactor search bar button event handling

--- app/components/CompactAppSearchBar.vue
+++ app/components/CompactAppSearchBar.vue
@@ -1,22 +1,11 @@
 <script lang="ts" setup>
 const searchOpened = ref(false);
-
-
-const onMagnifyClick = () => {
-  searchOpened.value = true;
-}
-
-const onBackClick = () => {
-  searchOpened.value = false;
-}
 </script>
 
 <template>
   <v-row dense align="center">
     <v-col v-if="!searchOpened" align="right">
-      <v-btn icon variant="text" @click="onMagnifyClick">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <v-btn icon="mdi-magnify" variant="text" @click="searchOpened = true" />
     </v-col>
 
     <div v-if="searchOpened" class="position-absolute top-0 left-0 right-0 bottom-0">
@@ -24,9 +13,7 @@ const onBackClick = () => {
         <v-container>
           <v-row dense align="center">
             <v-col cols="auto">
-              <v-btn icon variant="text" @click="onBackClick">
-                <v-icon>mdi-arrow-left</v-icon>
-              </v-btn>
+              <v-btn icon="mdi-arrow-left" variant="text" @click="searchOpened = false" />
             </v-col>
 
             <v-col align="center">
