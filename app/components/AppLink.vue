32a72253f1373f9713880ef638bda813cc399917
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-01-18 20:39:08 -0300

    Refactor VideoThumbnail and add AppLink component

---
+++ app/components/AppLink.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  to: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to="to" style="color: inherit; text-decoration: none;">
+    <slot />
+  </NuxtLink>
+</template>
--- app/components/VideoThumbnail.vue
+++ app/components/VideoThumbnail.vue
@@ -8,16 +8,14 @@ const props = defineProps<{
   tonal?: boolean;
 }>();
 
-const router = useRouter();
-
 const createdDate = ref<Date | null>(props.video.created ? new Date(props.video.created) : null);
 const isNewRelease = ref<boolean>(
   createdDate.value
     ? (new Date().getTime() - createdDate.value.getTime()) / (1000 * 60 * 60 * 24) <= 14
     : false
 );
-const colsThumbnail = ref<number>(props.vertical ? 12 : 5);
-const colsInfo = ref<number>(props.vertical ? 12 : 7);
+const colsThumbnail = ref<number>(props.vertical ? 12 : 6);
+const colsInfo = ref<number>(props.vertical ? 12 : 6);
 const cardTextAlign = ref<string>(props.vertical ? 'text-center' : '');
 
 const cardVariant = computed<any>(() => (props.tonal ? 'tonal' : 'elevated'));
@@ -37,34 +35,34 @@ const url = computed<string>(() => {
 </script>
 
 <template>
-  <v-card @click.prevent="!disabled && router.push(url)" :href="url" :disabled="disabled" :variant="cardVariant">
-    <v-row no-gutters align="center">
+  <v-card :to="url" :variant="cardVariant" :class="cardTextAlign" :disabled="props.disabled" hoverable>
+    <v-row no-gutters>
       <v-col :cols="colsThumbnail">
-        <v-img :src="props.video.thumbnail" :alt="`Thumbnail for ${props.video.title}`" :aspect-ratio="16 / 9" cover />
+        <v-img :src="props.video.thumbnail" :alt="`Thumbnail for ${props.video.title}`" :aspect-ratio="16 / 9" />
       </v-col>
 
-      <v-col :cols="colsInfo">
-        <v-card-item :class="cardTextAlign">
-          <p class="text-truncate" :title="props.video.title">
-            <v-icon v-if="isNewRelease" icon="mdi-new-box" />
-            <v-icon v-if="props.video.explicit" icon="mdi-alpha-e-box" />
-            {{ props.video.title }}
-          </p>
+      <v-col :cols="colsInfo" align-self="center" class="pa-2">
+        <p class="text-truncate">
+          <v-icon v-if="isNewRelease" icon="mdi-new-box" />
+          <v-icon v-if="props.video.explicit" icon="mdi-alpha-e-box" />
+          {{ video.title }}
+        </p>
 
-          <p class="text-truncate text-caption">
-            <ArtistLink :artists="props.video.artists" />
-          </p>
+        <p class="text-truncate text-caption">
+          <template v-for="(artist, index) in props.video.artists" :key="artist.id">
+            <AppLink :to="`/artist/${artist.artist.id}`">
+              {{ artist.artist.name }}
+            </AppLink>
+            <span v-if="(index as number) < props.video.artists.length - 1">, </span>
+          </template>
+        </p>
 
-          <p class="text-truncate text-caption">
-            <template v-if="props.video.duration">
-              {{ formatDuration(props.video.duration) }}
-            </template>
-
-            <template v-if="props.video.viewCounts?.total">
-              • {{ props.video.viewCounts.total.toLocaleString() }} views
-            </template>
-          </p>
-        </v-card-item>
+        <p class="text-truncate text-caption">
+          {{ formatDuration(props.video.duration) }}
+          <span v-if="props.video.viewCounts?.total">
+            • {{ props.video.viewCounts.total.toLocaleString() }} views
+          </span>
+        </p>
       </v-col>
     </v-row>
   </v-card>
