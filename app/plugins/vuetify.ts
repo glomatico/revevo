aea06f9582ea509daf15cc3b491b2f82125fe142
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-01-18 23:47:48 -0300

    Remove unused md3 blueprint import from vuetify plugin

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -3,8 +3,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
-import { md3 } from 'vuetify/blueprints'
-
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
